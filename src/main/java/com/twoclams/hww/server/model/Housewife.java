54faba8ca3cc27509b302bc8836c76ec004771b9
Federico Wachs <federico.wachs@2clams.com>
2013-01-23 14:49:39 -0300

    qw
    Signed-off-by: Federico Wachs <federico.wachs@2clams.com>

--- src/main/java/com/twoclams/hww/server/model/Housewife.java
+++ src/main/java/com/twoclams/hww/server/model/Housewife.java
@@ -112,7 +112,7 @@ public class Housewife implements Serializable {
     }
 
     public Integer getLastMissionId() {
-        if (lastMissionId == null) {
+        if (lastMissionId == null || new Integer(0).equals(lastMissionId)) {
             return new Integer(1);
         }
         return lastMissionId;
@@ -120,7 +120,7 @@ public class Housewife implements Serializable {
 
     public void setLastMissionId(Integer lastMissionId) {
         if (lastMissionId == null || new Integer(0).equals(lastMissionId)) {
-            this.lastMissionId = 1;
+            this.lastMissionId = new Integer(1);
         }
         this.lastMissionId = lastMissionId;
     }
